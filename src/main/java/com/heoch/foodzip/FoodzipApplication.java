e3a317ccf3e7bed526e6b2f2bafe138bfd2d19ed
ohch <alexohjr@naver.com>
2023-01-08 22:41:33 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/heoch/foodzip/FoodzipApplication.java
@@ -0,0 +1,13 @@
+package com.heoch.foodzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodzipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodzipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heoch/foodzip/FoodzipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heoch.foodzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodzipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
