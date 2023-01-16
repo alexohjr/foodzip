907284a096e27213f2d4ec49d1f6b57b43344861
ohch <alexohjr@naver.com>
2023-01-16 15:04:45 +0900

    사이드바 UI 및 토글버튼 변경 v5.0

--- src/main/resources/static/js/common/common.js
+++ src/main/resources/static/js/common/common.js
@@ -1,47 +1,35 @@
-let toggleBtnIcon = $('#sidebar-toggle-btn i');
+const sidebarToggleBtn = $('#sidebar-toggle-btn');
+const toggleBtnIcon = $('#sidebar-toggle-btn i');
+const sidebar = $('#sidebar');
+const sidebarMenu = $('#sidebar button.nav-link');
+const sidebarContent = $('.tab-content');
+
+let sidebarElemArr = [sidebar, sidebarToggleBtn, sidebarContent];
 
 $(function() {
-    $("#sidebar-toggle-btn").click(function() {
+    sidebarActiveTrigger();
 
-        toggleBtnIcon.toggleClass('bx-chevron-left');
-        toggleBtnIcon.toggleClass('bx-chevron-right');
-        $("#sidebar-tab-area, #sidebar").toggleClass("active");
-        $('.tab-content').toggleClass('active');
-        $(this).toggleClass("active");
+    sidebarToggleBtn.click(function() {
+        sidebar.trigger('classChanged');
     });
 
-    $('#sidebar button.nav-link').click(function() {
-
-        if($(this).hasClass('orange') && $('#sidebar').hasClass('active')) {
-            $("#sidebar").removeClass("active");
-            $('.tab-content').removeClass('active');
-            $('#sidebar-toggle-btn').removeClass('active');
-        } else if(!$(this).hasClass('orange') && !$('#sidebar').hasClass('active')) {
-            $("#sidebar").addClass("active");
-            $('.tab-content').addClass('active');
-            $('#sidebar-toggle-btn').addClass('active');
-            $('#sidebar button.nav-link').removeClass('orange');
-            $(this).addClass('orange');
-        } else if($(this).hasClass('orange') && !$('#sidebar').hasClass('active')) {
-            $("#sidebar").addClass("active");
-            $('.tab-content').addClass('active');
-            $('#sidebar-toggle-btn').addClass('active');
-        } else {
-            $('#sidebar button.nav-link').removeClass('orange');
-            $(this).addClass('orange');
+    sidebarMenu.click(function() {
+        if($(this).hasClass('active-menu') || !sidebar.hasClass('active')) {
+            sidebar.trigger('classChanged');
         }
+        sidebarMenu.removeClass('active-menu');
+        $(this).addClass('active-menu');
     });
-
-    $('#sidebar button.nav-link').not('active').click(function() {
-
-        if($('#sidebar-toggle-btn').hasClass('active')) {
-            toggleBtnIcon.addClass('bx-chevron-left');
-            toggleBtnIcon.removeClass('bx-chevron-right');
-        } else {
-            toggleBtnIcon.addClass('bx-chevron-right');
-            toggleBtnIcon.removeClass('bx-chevron-left');
-        }
-
-    });
-
 });
+
+
+function sidebarActiveTrigger() {
+
+    sidebar.on('classChanged', function() {
+        sidebarElemArr.forEach(function(elem) {
+            elem.toggleClass('active');
+        });
+        toggleBtnIcon.toggleClass('bx-chevron-left, bx-chevron-right');
+    });
+
+}
