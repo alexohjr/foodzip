b59a4bc40d030ed0345dc957d34a4da3e9776345
ohch <alexohjr@naver.com>
2023-01-17 13:40:02 +0900

    사이드바 토글버튼 ui 변경

--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -1,10 +1,12 @@
-   $(function() {
-        console.log("123456");
+$(function() {
 
-        var mapOptions = {
-          center: new naver.maps.LatLng(37.3595704, 127.105399),
-          zoom: 10
-      };
+     var mapOptions = {
+          center: new naver.maps.LatLng(37.49918743412937, 127.03455133162683),
+          // center: new naver.maps.LatLng(99.19694482, 218.33569082),
+          zoom: 17, // 줌 설정, 수치가 클수록 지도 확대
+          zoomControl: true
+     };
 
-          var map = new naver.maps.Map('map', mapOptions);
+
+     var map = new naver.maps.Map('map', mapOptions);
    });
